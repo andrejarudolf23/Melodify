4725983efcc7b4815eeb5706df4e08f5198d88fe
andrejarudolf23 <57088930+andrejarudolf23@users.noreply.github.com>
2019-11-01 21:05:50 +0100

    Finishing left side of Now Playing bar

--- index.php
+++ index.php
@@ -29,6 +29,16 @@ else {
 					<span class="albumLink">
 						<img src="https://www.browntrout.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/9/7/9781465085481_CVR.jpg" class="albumArtwork">
 					</span>
+
+					<div class="trackDetails">
+						<span class="songName">
+							<span>Lose Yourself</span>
+						</span>
+						<span class="songAuthor">
+							<span>Eminem</span>
+						</span>
+					</div>
+
 				</div>
 			</div>
 			<div id="nowPlayingCenter">
